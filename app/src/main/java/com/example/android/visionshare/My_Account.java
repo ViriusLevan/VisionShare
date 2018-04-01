41ca8de2cd0a907e8e4140d08fc144e82252c054
ViriusLevan <Mantor562@gmail.com>
2018-04-01 22:02:56 +0700

    Tabs on My_Account
    Added My_Account_Projects, My_Account_Info

--- app/src/main/java/com/example/android/visionshare/My_Account.java
+++ app/src/main/java/com/example/android/visionshare/My_Account.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class My_Account extends Fragment {
 
@@ -16,13 +21,25 @@ public class My_Account extends Fragment {
     public View onCreateView (LayoutInflater inflater,ViewGroup container,
                               Bundle savedInstanceState){
 
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_account,container, false);
+        // Setting ViewPager for each Tabs
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) view.findViewById(R.id.my_account_tabs);
+        tabs.setupWithViewPager(viewPager);
+
+
+        return view;
+
+        //return inflater.inflate(R.layout.fragment_my_account, container, false);
 
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
+        /*
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) getView().findViewById(R.id.viewpager);
         viewPager.setAdapter(new FragmentTabPageAdapter(getFragmentManager(),
@@ -31,7 +48,7 @@ public class My_Account extends Fragment {
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = (TabLayout) getView().findViewById(R.id.my_account_tabs);
         tabLayout.setupWithViewPager(viewPager);
-
+        */
     }
 
     @Override
@@ -50,5 +67,47 @@ public class My_Account extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    // Add Fragments to Tabs
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(My_Account_Info.newInstance(), "My Info");
+        adapter.addFragment(My_Account_Projects.newInstance(), "My Projects");
+        viewPager.setAdapter(adapter);
+
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
 
 }
