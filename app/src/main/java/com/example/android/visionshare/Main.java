6592b766dfc7391c20a1ca54d183fa4a51bd371c
ViriusLevan <Mantor562@gmail.com>
2018-04-01 17:17:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/visionshare/Main.java
@@ -0,0 +1,51 @@
+package com.example.android.visionshare;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Main extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_trending:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_news:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_submission:
+                    mTextMessage.setText(R.string.title_submission);
+                    return true;
+                case R.id.navigation_my_account:
+                    mTextMessage.setText(R.string.title_my_account);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigationView);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/android/visionshare/Submission.java
@@ -0,0 +1,4 @@
+package com.example.android.visionshare;
+
+public class Submission {
+}
