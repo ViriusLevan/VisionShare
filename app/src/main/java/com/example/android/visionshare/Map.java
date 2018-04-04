d82c694d16fd3bf43afedf3ec8be679517c44d84
matemo22 <matemo2204@gmail.com>
2018-04-04 22:16:49 +0700

    google map

--- app/src/main/java/com/example/android/visionshare/Map.java
+++ app/src/main/java/com/example/android/visionshare/Map.java
@@ -1,11 +1,13 @@
 package com.example.android.visionshare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class Map extends Fragment {
@@ -19,6 +21,14 @@ public class Map extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.fragment_map, container, false);
+        Button button = view.findViewById(R.id.map_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent b = new Intent(view.getContext(), MapsActivity.class);
+                startActivity(b);
+            }
+        });
         ListView newsList = view.findViewById(R.id.map_news_list);
         ListView trendingList = view.findViewById(R.id.map_trending_list);
         String[] news = {"Heboh naga di Tulung Agung", "Ikan Indosiar Mati", "[HELP] Desa Benowo sebagai tempat wisata"};
