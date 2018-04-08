1191b6c41049c21f195b51c35db03868dbab2c84
kevinkho1996 <33369254+kevinkho1996@users.noreply.github.com>
2018-04-08 14:57:24 +0700

    UI ver 1.2

--- app/src/main/java/com/example/android/visionshare/Submission.java
+++ app/src/main/java/com/example/android/visionshare/Submission.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +22,8 @@ public class Submission extends Fragment{
 
 
     View view;
+    ImageView imageView;
+    Button button;
     public static Submission newInstance() {
         Submission fragment = new Submission();
         return fragment;
