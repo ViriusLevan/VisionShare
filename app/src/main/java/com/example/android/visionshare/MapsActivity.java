611dd35f67d7b18f29e53eed3cfa9db234191a99
matemo22 <matemo2204@gmail.com>
2018-04-09 18:10:52 +0700

    google map auto zoom

--- app/src/main/java/com/example/android/visionshare/MapsActivity.java
+++ app/src/main/java/com/example/android/visionshare/MapsActivity.java
@@ -1,30 +1,38 @@
 package com.example.android.visionshare;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    double LAT = -34, LON = 151;
+    Place place;
+    private Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        setUpMapIfNeeded();
 
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
@@ -33,7 +41,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onPlaceSelected(com.google.android.gms.location.places.Place place) {
+                LatLng position = place.getLatLng();
+                MarkerOptions markerOptions = new MarkerOptions().
+                        position(position).
+                        title((String)place.getName()).
+                        snippet("Your last recorded location");
 
+                marker = mMap.addMarker(markerOptions);
+
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(position, 15);
+                mMap.animateCamera(cameraUpdate);
             }
 
             @Override
@@ -44,13 +61,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        double lat = LAT;
+        double lng = LON;
+        String name = "You are here";
+        if (place != null) {
+            LatLng latLng = place.getLatLng();
+            lat = latLng.latitude;
+            lng = latLng.longitude;
+            name = (String)place.getName();
+        }
+        if (marker != null) {
+            marker.remove();
+        }
+        LatLng position = new LatLng(lat, lng);
+        MarkerOptions markerOptions = new MarkerOptions().
+                position(position).
+                title(name).
+                snippet("Your last recorded location");
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        marker = mMap.addMarker(markerOptions);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(position, 15);
+        mMap.animateCamera(cameraUpdate);
     }
+
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
 }
